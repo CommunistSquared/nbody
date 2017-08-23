f5bd2a992141f6aba2df53fff9d7015e791710a5
anton <anton@DESKTOP-BVOLBCE>
2017-08-22 20:07:29 -0700

    cleaned up a bit

--- src/nbody/Nbody.java
+++ src/nbody/Nbody.java
@@ -3,25 +3,26 @@ package nbody;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.Dimension;
 
 public class Nbody extends JPanel implements ActionListener {
 
+    // Index of the body that camera is centered on
+    int cameraIndex = 0;
+    double scale = 1.0;
     int frame = 0;
     BSim sim;
     Timer timer = new Timer(16, this);
     BPoint camera = new BPoint(0, 0);
-                   Body[] inputBodies = new Body[3];
-                //x, y, mass, name, dir, force, trail length
-//                inputBodies[0] = new Body(200.0, 500.0, 100.0, "planet1", 0.0, 0.2, 500);
-//                inputBodies[1] = new Body(200.0, 200.0, 10.0, "moon1", 0.0, 2.3, 500);
-//                inputBodies[2] = new Body(200.0, 150.0, 1.0, "meteor1", 0.0, 0.8, 500);
+    //x, y, mass, name, dir, speed, trail length   
+    static Body[] inputBodies = new Body[]{
+        new Body(200.0, 500.0, 100.0, "planet1", 0.0, 0.2, 500),
+        new Body(200.0, 200.0, 10.0, "moon1", 0.0, 2.3, 500),
+        new Body(200.0, 150.0, 1.0, "meteor1", 0.0, 0.8, 500)
+    };
+    static Dimension size = new Dimension(1000, 1000);
 
     //creates BSim when called
     public Nbody(Body[] inputBodies, double inputGravConstant) {
-                        inputBodies[0] = new Body(200.0, 500.0, 100.0, "planet1", 0.0, 0.2, 500);
-                inputBodies[1] = new Body(200.0, 200.0, 10.0, "moon1", 0.0, 2.3, 500);
-                inputBodies[2] = new Body(200.0, 150.0, 1.0, "meteor1", 0.0, 0.8, 500);
         sim = new BSim(inputBodies, inputGravConstant);
         timer.start();
     }
@@ -53,13 +54,13 @@ public class Nbody extends JPanel implements ActionListener {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         for (int a = 0; a < sim.getBodies().length; a++) {
-            camera = sim.getBodies()[2].getBPoint();
+            camera = sim.getBodies()[cameraIndex].getBPoint();
             Body body = sim.getBodies()[a];
             String name = body.getName();
             int mass = (int) body.getMass();
             int diameter = (int) Math.sqrt(body.getMass());
-            int camX = (int) camera.getX() - 500;
-            int camY = (int) camera.getY() - 500;
+            int camX = (int) camera.getX() - (int)(size.getWidth() / 2);
+            int camY = (int) camera.getY() - (int)(size.getHeight() / 2);
             int x = (int) body.getX() - camX;
             int y = (int) body.getY() - camY;
             int trailLength = (int) body.getTrail().length;
@@ -77,17 +78,12 @@ public class Nbody extends JPanel implements ActionListener {
         }
     }
 
-    //this is called first when i run this program, this is also where i create the actual bodies to simulate
+    //this is called first when i run this program
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-//                Body[] inputBodies = new Body[3];
-//                //x, y, mass, name, dir, force, trail length
-//                inputBodies[0] = new Body(200.0, 500.0, 100.0, "planet1", 0.0, 0.2, 500);
-//                inputBodies[1] = new Body(200.0, 200.0, 10.0, "moon1", 0.0, 2.3, 500);
-//                inputBodies[2] = new Body(200.0, 150.0, 1.0, "meteor1", 0.0, 0.8, 500);
-                createSim(1000, 1000, inputBodies, -10.0);
+                createSim((int)size.getWidth(), (int)size.getHeight(), inputBodies, -10.0);
             }
         });
     }
